bd314ec93671b175ddae0132bf3a8a1561fd9ebc
Devon <devonlee111@gmail.com>
2023-03-10 10:51:32 -0800

    Uploader Example

--- whisper-server/routes/index.js
+++ whisper-server/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const multer  = require('multer');
-const upload = multer({ dest: os.tmpdir() });
+const upload = multer({ dest: './uploads' });
 
 router.post('/upload', upload.single('file'), function(req, res) {
   const title = req.body.title;
