1c815e5d658b5dbf724def6c1b999616089f075e
Devon Lee <devonlee111@gmail.com>
2023-03-10 18:03:16 -0800

    Add Console Logs

--- whisper-server/whisper/whisper.js
+++ whisper-server/whisper/whisper.js
@@ -32,10 +32,13 @@ class transcriber {
 
 	// Forever process to watch queue for newly queued file to transcribe
 	async startTranscriptionProcess() {
+		console.log("staring transcription process...");
 		while (true) {
 			await sleep(100);
 			if (this.transcriptionQueue.length > 0) {
+				console.log("queue is not empty, starting transcription process...");
 				file = this.transcriptionQueue.shift();
+				console.log(`beginning transcription of ${file}`);
 				this.transcribe(file);
 			}
 		}
@@ -43,7 +46,9 @@ class transcriber {
 
 	// Transcribe a given file with whisper
 	transcribe(file) {
-		exec(`whisper ${file} --model medium`, (error, stdout, stderr) => {
+		let command = `whisper ${file} --model medium`;
+		console.log(`executing whisper command: ${command}`);
+		exec(command, (error, stdout, stderr) => {
 			if (error) {
 				console.log(`error: ${error.message}`);
 				return;
@@ -62,4 +67,4 @@ function sleep(time) {
 	return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-module.exports = { transcriber }
+module.exports = { transcriber };
