d1a678cc27c9a4299f8b00536b5da92161070115
Devon Lee <devonlee111@gmail.com>
2023-03-10 18:49:14 -0800

    Print All Whisper Output

--- whisper-server/whisper/whisper.js
+++ whisper-server/whisper/whisper.js
@@ -56,11 +56,9 @@ class transcriber {
 		exec(command, (error, stdout, stderr) => {
 			if (error) {
 				console.log(`error: ${error.message}`);
-				return;
 			}
 			if (stderr) {
 				console.log(`stderr: ${stderr}`);
-				return;
 			}
 			console.log(`stdout: ${stdout}`);
 		});
