0150d067de4d24785c35424c1d2922dcaaafca20
Devon Lee <devonlee111@gmail.com>
2023-03-15 10:05:52 -0700

    Save File Improvements

--- whisper-server-python/server.py
+++ whisper-server-python/server.py
@@ -1,15 +1,16 @@
 import os
-try:
-    import http.server as server
-except ImportError:
-    # Handle Python 2.x
-    import SimpleHTTPServer as server
+import uuid
+import http.server as server
+
+PORT = 8000
+UPLOAD_DIR = "./uploads"
 
 class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
     """Extend SimpleHTTPRequestHandler to handle PUT requests"""
     def do_PUT(self):
         """Save a file following a HTTP PUT request"""
-        filename = os.path.basename(self.path)
+        filename = str(uuid.uuid4())
+        filename = os.path.join(UPLOAD_DIR, filename)
 
         # Don't overwrite files
         if os.path.exists(filename):
@@ -19,13 +20,25 @@ class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
             self.wfile.write(reply_body.encode('utf-8'))
             return
 
+        # Write file
         file_length = int(self.headers['Content-Length'])
         with open(filename, 'wb') as output_file:
             output_file.write(self.rfile.read(file_length))
+
         self.send_response(201, 'Created')
         self.end_headers()
         reply_body = 'Saved "%s"\n' % filename
         self.wfile.write(reply_body.encode('utf-8'))
 
 if __name__ == '__main__':
-    server.test(HandlerClass=HTTPRequestHandler)
+    if not os.path.exists(UPLOAD_DIR):
+        os.makedirs(UPLOAD_DIR)
+
+    webServer = server.HTTPServer(("", PORT), HTTPRequestHandler(request, client_address, server))
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
