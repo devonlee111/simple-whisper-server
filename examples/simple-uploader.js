b2d7415cfdd844b5175c8588404ddbb3ac31f929
Devon <devonlee111@gmail.com>
2023-03-16 00:12:52 -0700

    Whisper Integration

--- examples/simple-uploader.js
+++ examples/simple-uploader.js
@@ -1,9 +1,15 @@
 const { exec } = require("child_process");
 
 let url = "http://localhost:8000/put";
-let file = "test.txt";
+let file = "test.mp3"
 
 exec(`curl -T ${file} ${url}`, (error, stdout, stderr) => {
-
+	if (error) {
+		console.log(`error: ${error.message}`);
+	}
+	if (stderr) {
+		console.log(`stderr: ${stderr}`);
+	}
+	console.log(`stdout: ${stdout}`);
 });
 
--- whisper-server/server.py
+++ whisper-server/server.py
@@ -1,9 +1,19 @@
 import os
+import time
 import uuid
 import http.server as server
+import whisper
+import threading
+from queue import Queue
 
 PORT = 8000
 UPLOAD_DIR = "./uploads"
+
+def transcribe(file):
+	model = whisper.load_model("base")
+	result = model.transcribe(file)
+	del model
+	return result["text"]
 
 class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
 	"""Extend SimpleHTTPRequestHandler to handle PUT requests"""
@@ -24,20 +34,27 @@ class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
 		with open(filename, 'wb') as output_file:
 			output_file.write(self.rfile.read(file_length))
 
+		"""Perform transcription"""
+		transcription = transcribe(filename)
+
+		"""Send response"""
 		self.send_response(201, 'Created')
 		self.end_headers()
-		reply_body = 'Saved "%s"\n' % filename
+		reply_body = transcription
 		self.wfile.write(reply_body.encode('utf-8'))
+
+		"""Cleanup"""
+		os.remove(filename)
 
 if __name__ == '__main__':
 	if not os.path.exists(UPLOAD_DIR):
 		os.makedirs(UPLOAD_DIR)
 
-		webServer = server.HTTPServer(("", PORT), HTTPRequestHandler)
-		try:
-			webServer.serve_forever()
-		except KeyboardInterrupt:
-			pass
+	webServer = server.HTTPServer(("", PORT), HTTPRequestHandler)
+	try:
+		webServer.serve_forever()
+	except KeyboardInterrupt:
+		pass
 
 	webServer.server_close()
 	print("Server stopped.")
